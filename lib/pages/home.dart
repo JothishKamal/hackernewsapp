30d362c213e288dcf7d32765bf1166fc2f824647
JothishKamal <jothishkamal@gmail.com>
2024-04-09 14:50:55 +0530

    add: sort by dropdown menu

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,22 +8,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> sortByList = ['Top', 'New', 'Best'];
+  String? sortMode;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(child: _searchField()),
-        ],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _searchField(),
+            const SizedBox(
+              height: 20,
+            ),
+            _sortByDB()
+          ],
+        ),
       ),
     );
   }
 
+  Container _sortByDB() {
+    return Container(
+            margin: const EdgeInsets.only(left: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.grey)),
+            child: DropdownButton<String>(
+                hint: const Text('Sort By'),
+                underline: Container(),
+                value: sortMode,
+                items: sortByList.map((String value) {
+                  return DropdownMenuItem<String>(
+                      value: value, child: Text(value));
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    sortMode = value;
+                  });
+                }),
+          );
+  }
+
   Container _searchField() {
     return Container(
-      margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+      margin: const EdgeInsets.only(top: 20, left: 10, right: 10),
       decoration: BoxDecoration(boxShadow: [
         BoxShadow(
             color: const Color(0xFF1D1617).withOpacity(0.11),

d743a956a4ad7754136bbd67b72467bcfaaa73f9
JothishKamal <jothishkamal@gmail.com>
2024-04-10 18:26:26 +0530

    add: story details

--- lib/pages/storypage.dart
+++ lib/pages/storypage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_browser/flutter_web_browser.dart';
 
 class StoryDetailsPage extends StatelessWidget {
   final Map<String, dynamic> result;
@@ -9,7 +10,59 @@ class StoryDetailsPage extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
       appBar: _appBar(),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                '${result['title']}',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  const Text(
+                    'URI: ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () => _launchURL(result['url']),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                        child: Text(
+                          '${result['url']}',
+                          style: const TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16,
+                            decoration: TextDecoration.underline,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+          ],
+        ),
+      ),
     ));
+  }
+
+  Future<void> _launchURL(String url) async {
+    await FlutterWebBrowser.openWebPage(url: url);
   }
 
   AppBar _appBar() {
@@ -17,7 +70,7 @@ class StoryDetailsPage extends StatelessWidget {
       centerTitle: true,
       backgroundColor: const Color.fromRGBO(255, 100, 4, 1),
       title: const Text(
-        'Hacker News',
+        'Story Details',
         style: TextStyle(
           color: Colors.black,
           fontSize: 28,

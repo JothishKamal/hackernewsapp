be7f542a06b7b6837c5dedfa7e0d7659c6fb5e46
JothishKamal <jothishkamal@gmail.com>
2024-04-10 20:12:45 +0530

    add: comments with indents

--- lib/pages/storypage.dart
+++ lib/pages/storypage.dart
@@ -1,5 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_browser/flutter_web_browser.dart';
+
+class Comment {
+  final String by;
+  final int id;
+  final List<int>? kids;
+  final int time;
+  final String text;
+  final int indentLevel;
+
+  Comment({
+    required this.by,
+    required this.id,
+    required this.kids,
+    required this.time,
+    required this.text,
+    required this.indentLevel,
+  });
+
+  factory Comment.fromJson(Map<String, dynamic> json, int indentLevel) {
+    return Comment(
+      by: json['by'] ?? '',
+      id: json['id'] ?? 0,
+      kids: json['kids'] != null ? List<int>.from(json['kids']) : null,
+      time: json['time'] ?? 0,
+      text: json['text'] ?? '',
+      indentLevel: indentLevel,
+    );
+  }
+}
 
 class StoryDetailsPage extends StatelessWidget {
   final Map<String, dynamic> result;
@@ -8,10 +40,9 @@ class StoryDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: _appBar(),
-      body: Center(
-        child: Column(
+      child: Scaffold(
+        appBar: _appBar(),
+        body: ListView(
           children: [
             Padding(
               padding: const EdgeInsets.all(10.0),
@@ -38,13 +69,12 @@ class StoryDetailsPage extends StatelessWidget {
                     child: GestureDetector(
                       onTap: () => _launchURL(result['url']),
                       child: Padding(
-                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
                         child: Text(
                           '${result['url']}',
                           style: const TextStyle(
                             color: Colors.blue,
                             fontSize: 16,
-                            decoration: TextDecoration.underline,
                           ),
                           overflow: TextOverflow.ellipsis,
                         ),
@@ -54,15 +84,120 @@ class StoryDetailsPage extends StatelessWidget {
                 ],
               ),
             ),
-
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  const Text(
+                    'Comments ',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black),
+                  ),
+                  Text(
+                    '${result['descendants']}',
+                    style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey),
+                  )
+                ],
+              ),
+            ),
+            _buildComments(result['kids'], 0),
           ],
         ),
       ),
-    ));
+    );
   }
 
-  Future<void> _launchURL(String url) async {
+  void _launchURL(String url) async {
     await FlutterWebBrowser.openWebPage(url: url);
+  }
+
+  Widget _buildComments(List<dynamic>? kids, int indentLevel) {
+    if (kids == null || kids.isEmpty) {
+      return Container();
+    }
+
+    return FutureBuilder<List<Comment>>(
+      future: _getCommentsByIds(kids, indentLevel),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: snapshot.data!.map<Widget>((comment) {
+              return _buildComment(comment);
+            }).toList(),
+          );
+        }
+      },
+    );
+  }
+
+  Future<List<Comment>> _getCommentsByIds(
+      List<dynamic> ids, int indentLevel) async {
+    List<Future<Comment>> futures = [];
+    for (var id in ids) {
+      futures.add(_getCommentById(id, indentLevel));
+    }
+    return await Future.wait(futures);
+  }
+
+  Future<Comment> _getCommentById(int id, int indentLevel) async {
+    final response = await http
+        .get(Uri.parse('https://hacker-news.firebaseio.com/v0/item/$id.json'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      return Comment.fromJson(data, indentLevel);
+    } else {
+      throw Exception('Failed to load comment');
+    }
+  }
+
+  Widget _buildComment(Comment comment) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              for (int i = 0; i < comment.indentLevel; i++)
+                Container(
+                  width: 1.0,
+                  height: 20.0,
+                  color: Colors.grey,
+                  margin: const EdgeInsets.only(right: 10.0),
+                ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      comment.by,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: Html(data: comment.text),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          _buildComments(comment.kids, comment.indentLevel + 1),
+        ],
+      ),
+    );
   }
 
   AppBar _appBar() {
